3bcb775c388a0cc79c9cc1c47913513fe3e85813
ricydubey <rishiduvey042@gmail.com>
2025-06-05 15:08:41 +0530

    fixed resume download functionaly

--- staticfiles/main/js/index.js
+++ staticfiles/main/js/index.js
@@ -4,9 +4,10 @@ toggle.addEventListener('click', () => {
   links.classList.toggle('active');
 });
 
-function downloadResume() {
+function downloadResume(button) {
+  const resumeUrl = button.getAttribute('data-resume-url');
   const link = document.createElement('a');
-  link.href = "{% static 'main/Resume.pdf' %}";
+  link.href = resumeUrl;
   link.download = 'Resume.pdf';
   document.body.appendChild(link);
   link.click();
