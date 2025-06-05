189f98770c0881d6104a5c7cb80b896c45852dc6
ricydubey <rishiduvey042@gmail.com>
2025-06-05 16:44:58 +0530

    Updated Function popup function again 2

--- staticfiles/main/js/index.js
+++ staticfiles/main/js/index.js
@@ -4,9 +4,11 @@ toggle.addEventListener('click', () => {
   links.classList.toggle('active');
 });
 
-function downloadResume() {
+
+function downloadResume(button) {
+  const resumeUrl = button.getAttribute('data-resume-url');
   const link = document.createElement('a');
-  link.href = "{% static 'main/resume.pdf' %}";
+  link.href = resumeUrl;
   link.download = 'Resume.pdf';
   document.body.appendChild(link);
   link.click();
